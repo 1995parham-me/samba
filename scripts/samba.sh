e2441248489e377e9ddd4587dc2d725db3b83804
Parham Alvani <parham.alvani@gmail.com>
2023-08-06 13:11:36 -0400

    feat: complete samba installation script

--- scripts/samba.sh
+++ scripts/samba.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 usage() {
-  echo "Samba is the standard Windows interoperability suite of programs for Linux and Unix"
+	echo "Samba is the standard Windows interoperability suite of programs for Linux and Unix"
 
-  # shellcheck disable=1004,2016
-  echo '
+	# shellcheck disable=1004,2016
+	echo '
                      _
  ___  __ _ _ __ ___ | |__   __ _
 / __|/ _` | |_ ` _ \| |_ \ / _` |
@@ -15,21 +15,17 @@ usage() {
 root=${root:?"root must be set"}
 
 main_pacman() {
-  return 1
-}
-
-main_apt() {
-  return 1
-}
-
-main_brew() {
-  return 1
+	require_pacman samba
 }
 
 main() {
-  return 0
+	return 0
 }
 
 main_parham() {
-  return 0
+	testparm "$root/smb.conf"
+	copycat "samba" "$root/smb.conf" /etc/samba/smb.conf
+
+	msg "Although the user name is shared with Linux system, Samba uses a password separate from that of the Linux user accounts."
+	sudo smbpasswd -a parham
 }
